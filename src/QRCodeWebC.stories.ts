4735b7a57a158f9f7506fd7dd4aeec3584688a5e
Gabriel Raducu <graducu@amerilife.com>
2023-10-06 10:58:39 +0300

    chore: improve stories
    chore: improve stories controls

--- src/QRCodeWebC.stories.ts
+++ src/QRCodeWebC.stories.ts
@@ -4,17 +4,41 @@ import { html } from "lit";
 
 const meta: Meta = {
   component: "qrcode-webc",
+  argTypes: {
+    errorCorrectionLevel: {
+      options: ['L', 'M', 'Q', 'H'],
+      control: 'radio'
+    }
+  }
 };
 export default meta;
 
 export const QRCode: StoryObj = {
   args: {
     value: "hello",
-    width: 200
+    width: 200,
+    errorCorrectionLevel: "L"
   },
   render: (args) =>
-    html`<qrcode-webc
-      value="${args.value}"
-      width="${args.width}"
-    ></qrcode-webc>`,
+    html`
+      <div style="display: flex; flex-direction: column; align-items: center;">
+        <qrcode-webc value="${args.value}" width="${args.width}"></qrcode-webc>
+        <section>
+          <p>
+            To use this in your project add <em>qrcode-webc</em> element into
+            your code
+          </p>
+          <p><strong>Required param: </strong>value</p>
+          <p><strong>Optional params: </strong>width, errorCorrectionLevel</p>
+          <p>
+            <strong>Github link: </strong
+            ><a href="https://github.com/eBitzu/qrcode-webc">qrcode-webc</a>
+          </p>
+          <p>
+            <strong>My portfolio: </strong
+            ><a href="https://gr-cv.vercel.app/">here</a>
+          </p>
+        </section>
+      </div>
+    `,
 };
